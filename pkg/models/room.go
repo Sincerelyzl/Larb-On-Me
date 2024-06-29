a6a45342f53433cf75c0bb858a1d151a87647773
idontknowtoobrother <idontknowtoobrother@gmail.com>
2024-06-29 19:30:36 +0700

    impl: model message

--- pkg/models/room.go
+++ pkg/models/room.go
@@ -1,1 +1,16 @@
 package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type (
+	Message struct {
+		UUID      primitive.Binary
+		UserUUID  primitive.Binary
+		Message   string
+		CreatedAt time.Time
+	}
+)
