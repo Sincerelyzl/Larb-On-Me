3ad2b9e817ccd60dc18a8afde8a65f1748c10577
Sincerelyzl <saklaw33@gmail.com>
2024-06-29 19:33:43 +0700

    Intialize room models

--- pkg/models/room.go
+++ pkg/models/room.go
@@ -1,1 +1,20 @@
 package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type (
+	Room struct {
+		UUID          primitive.Binary
+		Roomowneruuid primitive.Binary
+		Members       []primitive.Binary
+		Messages      []primitive.Binary
+		Joincode      string
+		DeletedAt     time.Time
+		CreatedAt     time.Time
+		UpdatedAt     time.Time
+	}
+)
