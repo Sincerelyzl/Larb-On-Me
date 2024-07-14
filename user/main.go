5d37e614596ea32a1b1997b4cbd2135cb937ce90
Sincerelyzl <saklaw33@gmail.com>
2024-07-14 21:43:54 +0700

    SetUp User Database

--- user/main.go
+++ user/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/Sincerelyzl/larb-on-me/common/database"
+	"github.com/Sincerelyzl/larb-on-me/common/models"
+	"github.com/Sincerelyzl/larb-on-me/user/repository"
+)
+
 func main() {
 
+	ctx := context.Background()
+	// Connect to Database Server
+	client, err := database.NewConnection(ctx, "mongodb://localhost:27018/")
+	if err != nil {
+		panic(err)
+	}
+
+	userCollection := client.Database("user_service").Collection("user")
+	userRepo := repository.NewMongoUserRepository(userCollection)
+
+	user := models.User{
+		Username: "Saksarak",
+		Password: "12345",
+	}
+	createdUser, err := userRepo.CreateUser(ctx, user)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(createdUser)
 }
