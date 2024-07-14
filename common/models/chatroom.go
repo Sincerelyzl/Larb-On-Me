cfc36e9574dc9bff5e0f23b1d29a182a8e3521c3
Sincerelyzl <saklaw33@gmail.com>
2024-07-15 00:32:04 +0700

    Create chatroom and message models

---
+++ common/models/chatroom.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type ChatRoom struct {
+	Uuid         primitive.Binary   `json:"uuid" bson:"uuid"`
+	UsersUuid    []primitive.Binary `json:"users_uuid" bson:"users_uuid"`
+	MessagesUuid []primitive.Binary `json:"messages_uuid" bson:"messages_uuid"`
+	Name         string             `json:"name" bson:"name"`
+	JoinCode     string             `json:"join_code" bson:"join_code"`
+	CreatedAt    time.Time          `json:"created_at" bson:"created_at"`
+	UpdatedAt    time.Time          `json:"updated_at" bson:"updated_at"`
+	DeletedAt    time.Time          `json:"deleted_at" bson:"deleted_at"`
+}
