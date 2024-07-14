d1ee3de8798b454df7e83d97acfff8630edeff72
c.jakkrit <jakkrit@gridwhiz.com>
2024-07-15 02:46:53 +0700

    feats: owned credentials token jaaa

--- user/go.mod
+++ user/go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/Sincerelyzl/larb-on-me/common v0.0.0-20240714145021-affa999334c1
 	github.com/gin-contrib/cors v1.7.2
 	github.com/gin-gonic/gin v1.10.0
-	github.com/go-playground/validator/v10 v10.20.0
 	go.mongodb.org/mongo-driver v1.16.0
 )
 
@@ -19,6 +18,7 @@ require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/uuid v1.6.0 // indirect
--- user/main.go
+++ user/main.go
@@ -1,50 +1,91 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"net/http"
 
-	"github.com/Sincerelyzl/larb-on-me/common/database"
-	"github.com/Sincerelyzl/larb-on-me/user/handler"
-	"github.com/Sincerelyzl/larb-on-me/user/httpserver"
-	"github.com/Sincerelyzl/larb-on-me/user/repository"
-	"github.com/Sincerelyzl/larb-on-me/user/usecase"
+	"github.com/Sincerelyzl/larb-on-me/common/middleware"
 )
+
+type test2 struct {
+	Message    string `json:"message"`
+	Connection string `json:"connection"`
+	Content    string `json:"content"`
+}
+type test struct {
+	Username string  `json:"username"`
+	Role     string  `json:"role"`
+	Name     string  `json:"name"`
+	Position string  `json:"position"`
+	Date     string  `json:"date"`
+	Age      int     `json:"age"`
+	Mores    []test2 `json:"mores"`
+}
 
 func main() {
 
-	// disable TLS verification.
-	http.DefaultTransport.(*http.Transport).TLSClientConfig.InsecureSkipVerify = true
-
 	// create context.
-	ctx := context.Background()
+	// ctx := context.Background()
 
-	// Connect to database server.
-	client, err := database.NewConnection(ctx, "mongodb://localhost:27018/")
+	userInput := test{
+		Username: "test",
+		Role:     "admin",
+		Name:     "test",
+		Position: "test",
+		Date:     "test",
+		Age:      20,
+		Mores: []test2{
+			{
+				Message:    "test",
+				Connection: "test",
+				Content:    "test",
+			},
+			{
+				Message:    "test",
+				Connection: "test",
+				Content:    "test",
+			},
+		},
+	}
+	encrypted, err := middleware.GenerateLOMKeys(userInput)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("\nencrypyed:" + encrypted)
+
+	claimsUser := test{}
+	err = middleware.ClaimsLOM(encrypted, &claimsUser)
 	if err != nil {
 		panic(err)
 	}
 
-	// Get all collection need to use.
-	userCollection := client.Database("user_service").Collection("users")
+	fmt.Println("\ndecrypted:" + fmt.Sprintf("%v", claimsUser))
+	// create claims.
 
-	// Create all repository to use.
-	userRepo := repository.NewMongoUserRepository(userCollection)
+	// // Connect to database server.
+	// client, err := database.NewConnection(ctx, "mongodb://localhost:27018/")
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	// Create all usecase to use.
-	userUseCase := usecase.NewUserUsecase(userRepo)
+	// // Get all collection need to use.
+	// userCollection := client.Database("user_service").Collection("users")
 
-	// Create all handler to use.
-	userHandler := handler.NewUserHandler(userUseCase)
+	// // Create all repository to use.
+	// userRepo := repository.NewMongoUserRepository(userCollection)
 
-	// Create all http server to use.
-	userHttpServer := httpserver.NewHTTPServer(userHandler)
+	// // Create all usecase to use.
+	// userUseCase := usecase.NewUserUsecase(userRepo)
 
-	// Run http server.
-	fmt.Println("User service is running on port 3008")
-	if err := userHttpServer.Run("3008"); err != nil {
-		panic(err)
-	}
+	// // Create all handler to use.
+	// userHandler := handler.NewUserHandler(userUseCase)
+
+	// // Create all http server to use.
+	// userHttpServer := httpserver.NewHTTPServer(userHandler)
+
+	// // Run http server.
+	// fmt.Println("User service is running on port 3008")
+	// if err := userHttpServer.Run("3008"); err != nil {
+	// 	panic(err)
+	// }
 
 }
