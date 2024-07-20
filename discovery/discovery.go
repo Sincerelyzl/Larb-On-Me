17535243b1fb966c5921701f0169a0e5ee354456
c.jakkrit <jakkrit@gridwhiz.com>
2024-07-21 06:27:14 +0700

    add interface registry

--- discovery/discovery.go
+++ discovery/discovery.go
@@ -1,1 +1,10 @@
 package discovery
+
+import "context"
+
+type Registry interface {
+	Register(ctx context.Context, instanceId, serviceName, hostPort string) error
+	Unregister(ctx context.Context, instanceId, serviceName string) error
+	Discover(ctx context.Context, serviceName string) ([]string, error)
+	HealthCheck(instanceId, serviceName string) error
+}
